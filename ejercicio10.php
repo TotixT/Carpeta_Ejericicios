b7f1252d1c6f2c062698cdaaf46a1eaf1de7fe5e
TotixT <santiagolopezgarcia22@gmail.com>
2023-05-17 09:31:13 -0500

    A

--- ejercicio10.php
+++ ejercicio10.php
@@ -16,8 +16,30 @@
     </form>
     <?php
         session_start();
-        if(isset($_POST["ejecutar"])){
-            
+        if (isset($_POST["ejecutar"])) {
+            if ($_POST["ejecutar"] == "Agregar número") {
+                $numero = $_POST["numero"];
+                if ($numero != 0) { 
+                    //$_session variable que se usa para almacenar datos en el web server(config, preferencias, detalles de login) 
+                    array_push($_SESSION["numeros"], $numero); 
+                }
+            } else if ($_POST["ejecutar"] == "Mostrar resultados") {
+                $numeros = $_SESSION["numeros"]; //$_SESSION["numeros"] = [10, 20]
+                $suma = array_sum($numeros);
+                $promedio = count($numeros) > 0 ? $suma / count($numeros) : 0; //if (?) y else (:)
+                $mayor = max($numeros);
+                $menor = min($numeros);
+                $contador = count($numeros);
+
+                echo "<h2>Resultados:</h2>";
+                echo "<p>La sumatoria de los valores es: {$suma}</p>";
+                echo "<p>El valor del promedio es: {$promedio}</p>";
+                echo "<p>Se ingresaron {$contador} valores</p>";
+                echo "<p>El mayor valor es: {$mayor}</p>";
+                echo "<p>El menor valor es: {$menor}</p>";
+            }
+        } else {
+            $_SESSION["numeros"] = []; 
         }
     ?>
 </body>
